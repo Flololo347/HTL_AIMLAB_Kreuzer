8e69ed9da2599af496cecbbce0fad81ca3394186
Fishstick721 <137979156+Fishstick721@users.noreply.github.com>
2023-09-27 14:39:04 +0200

    27_9_23_Mouse+Jump

--- HTL_AIMLAB_Kreuzer/Assets/Player/Playerscript.cs
+++ HTL_AIMLAB_Kreuzer/Assets/Player/Playerscript.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class Playerscript : MonoBehaviour
 {
+    public float jumpheight=200;
+    public float movementspeed=1;
+    public float MouseXspeed = 1;
+    Rigidbody rigit;
     // Start is called before the first frame update
     void Start()
     {
-        
+       rigit=GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
@@ -15,8 +19,13 @@ public class Playerscript : MonoBehaviour
     {
         float verticalInput=Input.GetAxis("Vertical");
         float horizontalInput=Input.GetAxis("Horizontal");
-
-        transform.position += transform.forward * verticalInput;
-        transform.position += transform.right * horizontalInput;
+        float mouseXinput = Input.GetAxis("Mouse X");
+        transform.position += transform.forward * verticalInput*movementspeed;
+        transform.position += transform.right * horizontalInput*movementspeed;
+        transform.Rotate(0, mouseXinput * MouseXspeed, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigit.AddForce(Vector3.up*jumpheight);
+        }
     }
 }
